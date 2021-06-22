cac82cfcb60e73f16cc104c9b467d1ac67b29cb0
Simon Richardson <stickupkid@gmail.com>
2021-06-22 10:35:08 +0100

    Managers: Add refactoring TODO
    Add note that both PackageManager and PackageCommander should be merged,
    the whole package is __very__ confusing to work with or expand upon.

--- manager/interface.go
+++ manager/interface.go
@@ -13,6 +13,11 @@ import (
 
 // PackageManager is the interface which carries out various
 // package-management related work.
+//
+// TODO (stickupid): Both the PackageManager and PackageCommander from the
+// commands package should be merged. The layout of the packaging package is
+// over-engineered. The commands should be placed directly into the package
+// types themselves and then the managers could be a lot more simpler.
 type PackageManager interface {
 	// InstallPrerequisite runs the command which installs the prerequisite
 	// package which provides repository management functionalityes.
